ef27ce8ae9d6e791a2e6fafe35f12044053df58c
ppp <piase1910@163.com>
2019-05-23 22:24:38 +0800

    [update]：添加flatmap，网络请求，以及flatmap下的错误处理

--- RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/ViewController.swift
@@ -10,8 +10,58 @@ import UIKit
 import RxCocoa
 import RxSwift
 
+struct User : Codable {
+    let name : String
+}
+
+struct Response:Codable {
+    let errorCode:Int
+    let user:User
+    static let LoginSuccess = 1
+}
+
+enum LoginError:Swift.Error {
+    case networkError
+    case loginInfoValid
+}
+
+
+func login(userName:String,passWord:String) -> Single<Response> {
+    return  Single<Response>.create(subscribe: { (single) -> Disposable in
+        var request = URLRequest.init(url: URL.init(string: "http://127.0.0.1:8080/login")!)
+        request.httpMethod = "POST"
+        let info = ["username":userName,
+                    "password":passWord]
+        
+        let data = try? JSONEncoder().encode(info)
+        request.httpBody = data
+        
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            if let data = data,
+                let response = try? JSONDecoder().decode(Response.self, from: data){
+                if response.errorCode != Response.LoginSuccess{
+                    single(SingleEvent.error(LoginError.loginInfoValid))
+                }else{
+                    single(SingleEvent.success(response))
+                }
+            }else{
+                single(SingleEvent.error(LoginError.networkError))
+            }
+        })
+        task.resume()
+        return Disposables.create {
+            task.cancel()
+        }
+    })
+}
+
+
 class ViewController: UIViewController {
 
+    enum error:Swift.Error {
+        case invalidUserName
+    }
+    
     @IBOutlet weak var passWordField: UITextField!
     
     @IBOutlet weak var userNameField: UITextField!
@@ -27,8 +77,10 @@ class ViewController: UIViewController {
     }
 
     private func bindView(){
-        let passWordInput = self.passWordField.rx.text
+        
         let userNameInput = self.userNameField.rx.text
+        let passWordInput = self.passWordField.rx.text
+        
         let passWordValid = passWordInput.map { (passWord) -> Bool in
             if let passWord = passWord{
                 return passWord.count > 6 && passWord.count < 20
@@ -41,10 +93,35 @@ class ViewController: UIViewController {
             }
             return false
         }.debug("userNameValid")
+        
         let loginValid = Observable<Bool>.combineLatest(userNameValid, passWordValid) { (userName, passWord) -> Bool in
             return userName && passWord
         }
         loginValid.debug("loginValid").bind(to: self.loginButton.rx.isEnabled).disposed(by: bag)
+        
+        let loginInfo = Observable<(String,String)>.combineLatest(userNameInput, passWordInput) { (userNameText, passWordText) -> (String, String) in
+            return (userNameText ?? "",passWordText ?? "")
+        }
+        
+        let loginEvent = self.loginButton.rx.tap
+        
+        loginEvent.withLatestFrom(loginInfo).flatMap { (info) -> Observable<Event<Response>> in
+                let (userName,passWord) = info
+                return login(userName: userName,
+                             passWord: passWord)
+                    .do(onError: { (error) in
+                        //handle error
+                        print(error)
+                    }).asObservable().materialize()
+            }.debug("login")
+            .map { (event) -> User? in
+                //map next User? type
+                return event.element?.user
+            }.filter({ (u) -> Bool in
+                //filter nil model
+                return u != nil
+            })
+            .subscribe().disposed(by: bag)
     }
 
 }
